97ffdf53d62faa6e01c2f4b29b9eccf0f3735291
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2022-05-18 16:56:58 +0200

    delete old stuff from messages.h

--- src/messages.h
+++ src/messages.h
@@ -17,10 +17,6 @@
 
 #include "marshal.h"
 
-// todo: std tuple and usings instead of structs and thus easier serialisation?
-// but then using std::get everywhere would be a nigthmare, wouldn't it?
-// decomposition though?
-
 using PlayerId = uint8_t;
 using BombId = uint32_t;
 using Position = std::pair<uint16_t, uint16_t>;
@@ -44,27 +40,14 @@ enum Direction {
   UP, RIGHT, DOWN, LEFT,
 };
 
-// struct Join {
-//   std::string name;
-//   Join(const std::string& str) : name(str) {}
-//   Join() : name() {}
-// };
-
+// Join(name)
 using Join = std::string;
 
 // Placeholders.
 struct PlaceBomb {};
 struct PlaceBlock {};
 
-// using PlaceBomb = std::tuple<>;
-// using PlaceBlock = std::tuple<>;
-
-// struct Move {
-//   Direction direction;
-//   Move(Direction dir) : direction(dir) {}
-//   Move() : direction() {}
-// };
-
+// Move(direction)
 using Move = Direction;
 
 using ClientMessage = std::variant<Join, PlaceBomb, PlaceBlock, Move>;
@@ -74,71 +57,26 @@ using ClientMessage = std::variant<Join, PlaceBomb, PlaceBlock, Move>;
 namespace server_messages
 {
 
-// struct Player {
-//   std::string name;
-//   std::string address;
-// };
-
+// Player(name, address)
 using Player = std::pair<std::string, std::string>;
 
-// struct Bomb {
-//   Position position;
-//   uint16_t timer;
-//   bool operator==(const Bomb& other) const
-//   {
-//     return position == other.position;
-//   }
-
-//   auto operator<=>(const Bomb& other) const
-//   {
-//     return position <=> other.position;
-//   }
-// };
-
+// Bomb(position, timer)
 using Bomb = std::pair<Position, uint16_t>;
 
 enum EventType {
   BOMB_PLACED, BOMB_EXPLODED, PLAYER_MOVED, BLOCK_PLACED
 };
 
-// structures for those events:
-// struct BombPlaced {
-//   BombId id;
-//   Position position;
-
-//   bool operator==(const BombPlaced& other) const
-//   {
-//     return id == other.id;
-//   }
-
-//   auto operator<=>(const BombPlaced& other) const
-//   {
-//     return id <=> other.id;
-//   }
-// };
-
+// BombPlaced(id, position)
 using BombPlaced = std::pair<BombId, Position>;
 
-// struct BombExploded {
-//   BombId id;
-//   std::set<PlayerId> killed;
-//   std::set<Position> blocks_destroyed;
-// };
-
+// BombExploded(id, killed, destroyed)
 using BombExploded = std::tuple<BombId, std::set<PlayerId>, std::set<Position>>;
 
-// redundant perhaps?
-// struct BlockPlaced {
-//   Position position;
-// };
-
+// BlockPlaced(postion)
 using BlockPlaced = Position;
 
-// struct PlayerMoved {
-//   PlayerId id;
-//   Position position;
-// };
-
+// PlayerMoved(id, position)
 using PlayerMoved = std::pair<PlayerId, Position>;
 
 using Event = std::variant<BombPlaced, BombExploded, PlayerMoved, BlockPlaced>;
@@ -147,45 +85,21 @@ enum ServerMessageType {
   HELLO, ACCEPTED_PLAYER, GAME_STARTED, TURN, GAME_ENDED
 };
 
-// Hello
-// struct Hello {
-//   std::string server_name;
-//   uint8_t players_count;
-//   uint16_t size_x;
-//   uint16_t size_y;
-//   uint16_t game_length;
-//   uint16_t explosion_radius;
-//   uint16_t bomb_timer;
-// };
-
+// Hello(serv_name, players_count, size_x, size_y, game_length, radius, timer)
 using Hello =
   std::tuple<std::string, uint8_t, uint16_t, uint16_t, uint16_t, uint16_t, uint16_t>;
 
-// struct AcceptedPlayer {
-//   PlayerId id;
-//   Player player;
-// };
-
+// AcceptedPlayer(id, player)
 using AcceptedPlayer = std::pair<PlayerId, Player>;
 
-// also redundant?
-// struct GameStarted {
-//   std::map<PlayerId, Player> players;
-// };
-
+// GameStarted(players)
 using GameStarted = std::map<PlayerId, Player>;
 
-// struct Turn {
-//   uint16_t turn;
-//   std::vector<Event> events;
-// };
 
+// Turn(turnno, events)
 using Turn = std::pair<uint16_t, std::vector<Event>>;
 
-// struct GameEnded {
-//   std::map<PlayerId, Score> scores;
-// };
-
+// GameEnded(scores)
 using GameEnded = std::map<PlayerId, Score>;
 
 using ServerMessage = std::variant<Hello, AcceptedPlayer, GameStarted, Turn, GameEnded>;
@@ -199,35 +113,13 @@ enum DisplayMessageType {
   LOBBY, GAME
 };
 
-// struct Lobby {
-//   std::string server_name;
-//   uint8_t players_count;
-//   uint16_t size_x;
-//   uint16_t size_y;
-//   uint16_t game_length;
-//   uint16_t explosion_radius;
-//   uint16_t bomb_timer;
-//   std::map<PlayerId, server_messages::Player> players;
-// };
-
+// Lobby(serv_name, players_count, size_x, size_y, game_length, radius, timer, players)
 using Lobby =
   std::tuple<std::string, uint8_t, uint16_t, uint16_t, uint16_t, uint16_t, uint16_t,
              std::map<PlayerId, server_messages::Player>>;
 
-// struct Game {
-//   std::string server_name;
-//   uint16_t size_x;
-//   uint16_t size_y;
-//   uint16_t game_length;
-//   uint16_t turn;
-//   std::map<PlayerId, server_messages::Player> players;
-//   std::map<PlayerId, Position> player_positions;
-//   std::set<Position> blocks;
-//   std::set<server_messages::Bomb> bombs;
-//   std::set<Position> explosions;
-//   std::map<PlayerId, Score> scores;
-// };
-
+// Game(serv_name, size_x, size_y, game_length, turn, players, player_positions,
+//      blocks, bombs, explosions, scores)
 using Game =
   std::tuple<std::string, uint16_t, uint16_t, uint16_t, uint16_t,
              std::map<PlayerId, server_messages::Player>, std::map<PlayerId, Position>,
